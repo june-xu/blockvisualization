4fc528bae51f705475ed73a98b0069cf08b9f35f
Matthew <siunami.matt@gmail.com>
2018-02-16 23:33:36 -0700

    etherscan

---
+++ server.js
@@ -0,0 +1,22 @@
+// console.log("working")
+var Web3 = require('web3');
+var web3 = new Web3(new Web3.providers.HttpProvider());
+var version = web3.version.api;
+var jquery = require('jquery');
+
+var API_KEY = 'YEEWRXRQ8R2TN2ATBFTXWIF7UH81K4UR7D'
+
+jquery.getJSON('https://api.etherscan.io/api?module=contract&action=getabi&address=0xBB9bc244D798123fDe783fCc1C72d3Bb8C189413&apikey=' + API_KEY, function (data) {
+    var contractABI = "";
+    contractABI = JSON.parse(data.result);
+    if (contractABI != ''){
+        var MyContract = web3.eth.contract(contractABI);
+        var myContractInstance = MyContract.at("0x06012c8cf97BEaD5deAe237070F9587f8E7A266d");
+        var result = myContractInstance.memberId("0xfe8ad7dd2f564a877cc23feea6c0a9cc2e783715");
+        console.log("result1 : " + result);            
+        var result = myContractInstance.members(1);
+        console.log("result2 : " + result);
+    } else {
+        console.log("Error" );
+    }            
+});
