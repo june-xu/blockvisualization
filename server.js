06790a6cf1f9b8fa2c4cbd3f26bcce1a4b9dd130
Matthew <siunami.matt@gmail.com>
2018-02-17 00:26:05 -0700

    grabbing contract data

--- server.js
+++ server.js
@@ -11,12 +11,17 @@ app.use(express.static(path.join(__dirname, 'app', 'public')));
 app.use(bodyParser.json())
 app.set('port', (process.env.PORT || 5000))
 
+
 var API_KEY = 'YEEWRXRQ8R2TN2ATBFTXWIF7UH81K4UR7D'
 var api = require('etherscan-api').init(API_KEY);
 
 var getData = function(callback) {
-	var block = api.proxy.eth_getBlockByNumber(9);
+    // var blockNumber = 5105219;
+    // var hexString = blockNumber.toString(16);
+    var block = api.account.txlist('0x06012c8cf97BEaD5deAe237070F9587f8E7A266d');
     block.then(function(balanceData){
+        // console.log(balanceData);
+        // console.log(balanceData.result)
         callback(balanceData);
       });
 };
