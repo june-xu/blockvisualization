08d96f19899c77490d9df3233acc96f367abf55f
Matthew <siunami.matt@gmail.com>
2018-02-18 06:46:55 -0700

    working viz

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -276,6 +276,7 @@ class TestGraphs extends React.Component {
   constructor(props) {
       super(props);
       this.d3 = window.d3;
+      this.chart = window.Chart;
       this.state = { data: 1 }
   }
   componentDidUpdate(prevProps, prevState) {
@@ -313,21 +314,7 @@ class TestGraphs extends React.Component {
         byHour.shift();
         var lengthData = byHour.length;
         var data = byHour.splice(lengthData-10, lengthData);
-        var x = this.d3.scale.linear()
-            .domain([0, this.d3.max(data)])
-            .range([0, 420]);
-        
-        this.d3.select(".chart")
-            .selectAll("div").remove();
 
-        this.d3.select(".chart")
-            .selectAll("div")
-            .data(data)
-            .enter().append("div")
-            .attr("fill","black")
-            .style("width", function(d) { return x(d) + "px"; })
-            .attr("fill","white")
-            .text(function(d) { return d; });
     }
 }
   renderGraph() {
@@ -354,27 +341,146 @@ class TestGraphs extends React.Component {
           byHour.shift();
           var lengthData = byHour.length;
           var data = byHour.splice(lengthData-10, lengthData);
-          var x = this.d3.scale.linear()
-              .domain([0, this.d3.max(data)])
-              .range([0, 420]);
-
-          this.d3.select(".chart")
-              .selectAll("div")
-              .data(data)
-              .enter().append("div")
-              .attr("fill","black")
-              .style("width", function(d) { return x(d) + "px"; })
-              .attr("fill","white")
-              .text(function(d) { return d; });
+          var dataObject = {
+            labels: ["10hr","9hr","8hr","7hr","6hr","5hr","4hr","3hr","2hr","1hr",],
+            datasets: {
+              // label: "Transaction Time Series",
+              backgroundColor: ['#e6194b','#3cb44b','#ffe119','#0082c8','#f58231','#911eb4','#46f0f0','#f032e6','#d2f53c','#fabebe'],
+              borderColor: ['#e6194b','#3cb44b','#ffe119','#0082c8','#f58231','#911eb4','#46f0f0','#f032e6','#d2f53c','#fabebe'],
+              data: data
+            }
+          }
+          var ctx = document.getElementById('myChart').getContext('2d');
+          var myBarChart = new this.chart(ctx, {
+            type: 'bar',
+            data: dataObject,
+            options: {
+              scales: {
+                  xAxes: [{
+                      stacked: true
+                  }],
+                  yAxes: [{
+                      stacked: true
+                  }]
+              }
+          }
+            // options: options
+        });
+        }
       }
-  }
   render() {
       return (
           <div>
+              <canvas id="myChart"></canvas>
               <div className="chart"></div>
           </div>
       );
   }
 }
+
+// class TestGraphs extends React.Component {
+//   constructor(props) {
+//       super(props);
+//       this.d3 = window.d3;
+//       this.state = { data: 1 }
+//   }
+//   componentDidUpdate(prevProps, prevState) {
+//       {this.renderGraph2()}
+//   }
+//   componentDidMount() {
+//       fetch('http://localhost:5000/getTime').then(res=>res.json()).then(out=>{
+//           this.setState({
+//               data: out.result,
+//           })
+//           {this.renderGraph()}
+//       });
+//   }
+//   renderGraph2() {
+//     if (this.state.data == 1){
+//         return;
+//     } else {
+//         var d = this.state.data;
+//         var timestamps = [];
+//         var byHour = [];
+//         var currHour = [];
+//         var start;
+//         for (var x = 0; x < d.length; x++){
+//             if (typeof start == 'undefined'){
+//               start = d[x.timeStamp];
+//             }
+//             if (d[x].timeStamp - start < this.props.number*this.props.time){
+//                 currHour.push(d[x].timeStamp);
+//             } else {
+//                 byHour.push(currHour.length);
+//                 currHour = [];
+//                 start = d[x].timeStamp;
+//             }
+//         }
+//         byHour.shift();
+//         var lengthData = byHour.length;
+//         var data = byHour.splice(lengthData-10, lengthData);
+//         var x = this.d3.scale.linear()
+//             .domain([0, this.d3.max(data)])
+//             .range([0, 420]);
+        
+//         this.d3.select(".chart")
+//             .selectAll("div").remove();
+
+//         this.d3.select(".chart")
+//             .selectAll("div")
+//             .data(data)
+//             .enter().append("div")
+//             .attr("fill","black")
+//             .style("width", function(d) { return x(d) + "px"; })
+//             .attr("fill","white")
+//             .text(function(d) { return d; });
+//     }
+// }
+//   renderGraph() {
+//       if (this.state.data == 1){
+//           return;
+//       } else {
+//           var d = this.state.data;
+//           var timestamps = [];
+//           var byHour = [];
+//           var currHour = [];
+//           var start;
+//           for (var x = 0; x < d.length; x++){
+//               if (typeof start == 'undefined'){
+//                 start = d[x.timeStamp];
+//               }
+//               if (d[x].timeStamp - start < this.props.number*this.props.time){
+//                   currHour.push(d[x].timeStamp);
+//               } else {
+//                   byHour.push(currHour.length);
+//                   currHour = [];
+//                   start = d[x].timeStamp;
+//               }
+//           }
+//           byHour.shift();
+//           var lengthData = byHour.length;
+//           var data = byHour.splice(lengthData-10, lengthData);
+//           var x = this.d3.scale.linear()
+//               .domain([0, this.d3.max(data)])
+//               .range([0, 420]);
+
+//           this.d3.select(".chart")
+//               .selectAll("div")
+//               .data(data)
+//               .enter().append("div")
+//               .attr("fill","black")
+//               .style("width", function(d) { return x(d) + "px"; })
+//               .attr("fill","white")
+//               .text(function(d) { return d; });
+//       }
+//   }
+//   render() {
+//       return (
+//           <div>
+//               <div className="chart"></div>
+//           </div>
+//       );
+//   }
+// }
 
 export default App;
